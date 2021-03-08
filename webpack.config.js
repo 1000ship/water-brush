a506779ef8c50a054495d577965911d8fb070f73
1000ship <cjstjdgur1234@gmail.com>
2021-03-08 23:37:48 +0900

    refactor: Create static folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CpWebpackPlugin = require('cp-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: './src/index.js',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new CpWebpackPlugin({
+      from: path.resolve('./static'),
+      to: path.resolve('./dist'),
+    })
   ],
   devServer: {
     host: "0.0.0.0",
@@ -16,6 +20,7 @@ module.exports = {
     open: "Google Chrome",
     hot: true,
     compress: true,
+    contentBase: path.join(__dirname, 'dist'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
