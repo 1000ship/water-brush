18fc2119f7024ac361531756447b9d609e58445e
1000ship <cjstjdgur1234@gmail.com>
2021-03-08 13:53:12 +0900

    chore: Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: './src/index.js',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin()
+  ],
+  devServer: {
+    port: 5000,
+    open: "Google Chrome",
+    hot: true,
+    compress: true,
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      include: [ path.resolve(__dirname, 'src') ],
+      use: ['style-loader', 'css-loader']
+    }]
+  }
 };
