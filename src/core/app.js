bdb311fad1ae6782d4421752508660ae02872170
1000ship <cjstjdgur1234@gmail.com>
2021-03-08 12:12:03 +0900

    feat: Draw line

--- src/core/app.js
+++ src/core/app.js
@@ -1,14 +1,37 @@
 const PIXI = require("pixi.js");
 const { default: throttle } = require("../utils/throttle");
 
-const app = new PIXI.Application({ backgroundColor: 0x1099bb });
-document.body.appendChild(app.view);
 
-const fitToScreen = throttle(() => {
-  app.view.width = window.innerWidth;
-  app.view.height = window.innerHeight;
-}, 500);
-fitToScreen();
-window.addEventListener("resize", fitToScreen);
+class App extends PIXI.Application {
+  constructor({root, backgroundColor, ...props}) {
+    super({ backgroundColor, ...props });
+    root.appendChild(this.view);
+    
+    this.fitToScreen = throttle(() => {
+      this.renderer.view.style.width = window.innerWidth;
+      this.renderer.view.style.height = window.innerHeight;
+    }, 500);
+    
+    this.fitToScreen();
+    window.addEventListener("resize", this.fitToScreen); 
 
-module.exports = app
+  }
+  
+  set onEnterFrame ( onEnterFrame ) {
+    this._onEnterFrame = onEnterFrame
+    window.requestAnimationFrame(this._enterFrameHandler.bind(this))
+  }
+
+  get onEnterFrame ( ) {
+    return this._onEnterFrame
+  }
+
+  _enterFrameHandler () {
+    if( this.onEnterFrame ) {
+      this.onEnterFrame()
+      window.requestAnimationFrame(this._enterFrameHandler.bind(this))
+    }
+  }
+}
+  
+module.exports = App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 const PIXI = require("pixi.js");
-const app = require("./core/app");
+const App = require("./core/app");
 
+const app = new App({
+  root: document.body,
+  backgroundColor: 0x1099bb,
+})
 const graphics = new PIXI.Graphics();
 
 // Rectangle
-graphics.beginFill(0xde3249);
-graphics.drawRect(50, 50, 100, 100);
-graphics.endFill();
+// graphics.beginFill(0xde3249);
+// graphics.drawRect(0, 0, 100, 100);
+// graphics.endFill();
 
+let points = [[100,100], [200,300]]
+
+
+let prevX = null, prevY = null
 app.stage.addChild(graphics);
+app.ticker.add( delta => {
+  const {x, y} = app.renderer.plugins.interaction.mouse.global;
 
-const enterFrame = () => {
-  graphics.x += 1
-  requestAnimationFrame(enterFrame)
-}
-requestAnimationFrame(enterFrame)
+  points.push( [x,y] )
+  if( points.length > 100 )
+    points.splice( 0, 1 )
+  
+  graphics.clear()
+  graphics.moveTo( points[0][0], points[0][1] )
+  for( let i = 0; i < points.length; ++i )
+  {
+    graphics.lineStyle( 120 - (i / 10) * 10, 0xFFFFFF, i / 100, 0.5)
+    graphics.lineTo( points[i][0], points[i][1] )
+  }
+})
